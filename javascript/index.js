62785df05445e7432fa704ea689d98f67c9cac0d
maria <mpramos78915400@gmail.com>
2024-11-07 22:21:58 -0400

    callbacks

--- javascript/index.js
+++ javascript/index.js
@@ -30,3 +30,87 @@ const globalV='🌐'
     
 }
 
+// TIPOS DE DATOS PRIMITIVOS
+'susana' // texto| cadenas |string
+23423423   -2 -5 0  9.8 -7.5 // number
+true, false // booleanos
+null
+undefined
+
+let edad=24 //number
+
+// TIPOS DE DATOS COMPLEJOS
+// arreglos
+['a','ana','cesar']
+ [1,2,3,34,4,4]
+let edades=[24,56,34,56]
+console.log(edades);
+
+ // objetos
+{nombre:'juan', avatar:'👨'}
+
+let usuario={nombre:'juan', edad:39}
+console.log(usuario);
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+    
+}
+let cont=0
+while (cont<5) {
+    console.log(cont);
+    cont ++
+    
+}
+
+// funciones
+
+function decirHola() {
+    console.log('Hola a todos');
+    
+}
+decirHola()
+
+//callbacks
+let decirHola=function () {
+    console.log('hola desde una variable');
+    
+}
+
+decirHola()
+
+
+const sumar=(a,b)=>{
+    return a+b
+}
+const restar=(a,b)=>{
+        return a-b
+}
+function multiplicar(a,b) {
+    return a*b
+}
+
+function calculadora(callback,num1,num2) {
+    console.log(callback(num1,num2));
+}
+calculadora((a,b)=>a/b, 4,2)
+calculadora(sumar,3,4)
+calculadora(restar,5,4)
+calculadora(multiplicar,5,4)
+
+// Funciones puras
+
+for (let i = 0; i < emojis.length; i++) {
+  console.log(emojis[i]);
+}
+const emojis=['👨','😁','🤔']
+const mostrarEmojis= emojis.map((emoji)=>emoji)
+console.log(mostrarEmojis);
+
+
+
+
+
+
+
+
